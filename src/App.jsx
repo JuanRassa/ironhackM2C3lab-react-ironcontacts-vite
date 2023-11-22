e21194f8b47cf46417f3f065e2d650cc03476a55
Juan Rassa <jmrassa.dev@gmail.com>
2023-11-22 21:00:12 +0000

    Iteration 1: Render a table with the first 5 characters of the contacts data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import "./App.css";
+import { useState } from 'react';
+import contactsData from './contacts.json';
+import './App.css';
 
 function App() {
+  console.log(contactsData);
+  const [contacts, setContacts] = useState(contactsData.slice(0, 5));
+  console.log(contacts);
   return (
-    <div className="App">
+    <div className='App'>
       <h1>LAB | React IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <td>Picture</td>
+            <td>Name</td>
+            <td>Popularity</td>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map(contact => {
+            return (
+              <tr key={contact.id}>
+                <td>
+                  <img src={contact.pictureUrl} alt={contact.name} />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
